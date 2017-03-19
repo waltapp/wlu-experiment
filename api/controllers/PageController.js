baee3e297bf0c645a2d48490e584a834fb4d8731
gre.funston <mensatech@gmail.com>
2017-03-19 18:25:35 -0400

    add login and session functionality

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -1,57 +1,8 @@
-/**
- * PageController
- *
- * @description :: Server-side logic for managing pages
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-
 module.exports = {
-	
-
-
-  /**
-   * `PageController.page1()`
-   */
-  page1: function (req, res) {
-    return res.view('page/page1');
+  createAccount: function (req, res) {
+	  res.view('page/createAccount');
   },
-
-
-  /**
-   * `PageController.page2()`
-   */
-  page2: function (req, res) {
-    return res.view('page/page2');
-  },
-
-
-  /**
-   * `PageController.page3()`
-   */
-  page3: function (req, res) {
-    return res.json({
-      todo: 'page3() is not implemented yet!'
-    });
-  },
-
-
-  /**
-   * `PageController.page4()`
-   */
-  page4: function (req, res) {
-    return res.json({
-      todo: 'page4() is not implemented yet!'
-    });
-  },
-
-
-  /**
-   * `PageController.page5()`
-   */
-  page5: function (req, res) {
-    return res.json({
-      todo: 'page5() is not implemented yet!'
-    });
+  login:function (req, res) {
+	  res.view('page/login');
   }
 };
-
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,11 +1,30 @@
 /**
- * UserController
+ * UsersController
  *
  * @description :: Server-side logic for managing users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
 module.exports = {
-	
+  create: function (req, res) {
+	  //console.log(req);
+    if (req.query.password !== req.query.confirmPassword) {
+      return res.json(401, {err: 'Password doesn\'t match, What a shame!'});
+    }
+    var user={};
+    console.log(req.query.password);
+    user.password=req.query.password;
+    user.email=req.query.email;
+    User.create(user).exec(function (err, user) {
+      if (err) {
+        return res.json(err.status, {err: err});
+      }
+      // If user created successfuly we return user and token as response
+      if (user) {
+        // NOTE: payload is { id: user.id}
+        res.json(200, {user: user, token: jwToken.issue({id: user.id})});
+      }
+    });
+  }
+  
 };
-
